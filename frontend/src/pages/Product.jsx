c649c0ce973142d4cf36818ee644e5bb6d1c085b
Nahid-subi <scientisthassannahid@gmail.com>
2024-08-28 12:16:37 +0600

    cart functionality and order functionality done

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,9 +2,10 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import RelatedProducts from "./RelatedProducts";
 const Product = () => {
   const { productId } = useParams();
-  const { products, currency } = useContext(ShopContext)
+  const { products, currency, addToCart } = useContext(ShopContext)
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
   const [size, setSize] = useState('')
@@ -64,7 +65,7 @@ const Product = () => {
               ))}
             </div>
           </div>
-          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">ADD TO CART</button>
+          <button onClick={()=>addToCart(productData._id,size)} className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">ADD TO CART</button>
             <hr  className="mt-8 sm:w-4/5"/>
             <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
               <p>100% Original product.</p>
@@ -84,6 +85,9 @@ const Product = () => {
               <p>E-commerce websites typically display products or services along with detailed descriptions, images, prices, and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page with relevant information.</p>
           </div>
         </div>
+        {/* display related products */}
+
+        <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
     </div>
   ) : <div className="opacity-0"> </div>
 }
